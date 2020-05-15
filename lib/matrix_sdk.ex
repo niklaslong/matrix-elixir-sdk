6c0210177a0b6df73b845bd9c469c15d490166e9
nkls <niklas@long.ch>
2020-05-15 15:26:57 +0200

    remove hello world

--- lib/matrix_sdk.ex
+++ lib/matrix_sdk.ex
@@ -2,17 +2,4 @@ defmodule MatrixSDK do
   @moduledoc """
   Documentation for `MatrixSDK`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> MatrixSDK.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/matrix_sdk_test.exs
+++ test/matrix_sdk_test.exs
@@ -1,8 +1,4 @@
 defmodule MatrixSDKTest do
   use ExUnit.Case
   doctest MatrixSDK
-
-  test "greets the world" do
-    assert MatrixSDK.hello() == :world
-  end
 end
