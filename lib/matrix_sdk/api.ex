b6b60f529e0b321a3deb1d0fc7090735efd65478
nkls <niklas@long.ch>
2020-05-27 16:19:07 +0200

    add room discovery

--- lib/matrix_sdk/api.ex
+++ lib/matrix_sdk/api.ex
@@ -1,9 +1,24 @@
 defmodule MatrixSDK.API do
   @http_client Application.get_env(:matrix_sdk, :http_client)
 
+  # API Standards
+
   def spec_versions(client), do: @http_client.request(:get, client, "/_matrix/client/versions")
 
-  # TODO: response headers don't include json => middleware doesn't decode body
+  # Server discovery
+
+  # NOTE: response headers don't include json => middleware doesn't decode body
   def server_discovery(client),
     do: @http_client.request(:get, client, "/.well-known/matrix/client")
+
+  # User - Login
+
+
+  # User - registration
+
+  # Rooms
+
+  # TODO: handle chunked responses
+  # REVIEW: this works on matrix.org but not on local?
+  def room_discovery(client), do: @http_client.request(:get, client, "/_matrix/client/r0/publicRooms")
 end
--- test/matrix_sdk/api_test.exs
+++ test/matrix_sdk/api_test.exs
@@ -16,4 +16,15 @@ defmodule MatrixSDK.APITest do
 
     assert {:ok, _} = API.spec_versions(client)
   end
+
+
+  test "room_discovery/1: returns public rooms on server" do
+    client = HTTPClient.client("some_base_url.yay")
+
+    expect(HTTPClientMock, :request, fn :get, ^client, "/_matrix/client/r0/publicRooms" ->
+      {:ok, %Tesla.Env{}}
+    end)
+
+    assert {:ok, _} = API.room_discovery(client)
+  end
 end
