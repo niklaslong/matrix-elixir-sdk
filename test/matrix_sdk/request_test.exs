cc443343dea36fe7df7c7b1d53f49658e42b4ef1
nkls <niklas@long.ch>
2020-05-31 20:31:29 +0200

    refactor request tests

--- test/matrix_sdk/request_test.exs
+++ test/matrix_sdk/request_test.exs
@@ -3,73 +3,86 @@ defmodule MatrixSDK.RequestTest do
   alias MatrixSDK.Request
 
   test "spec_versions/1" do
-    request = Request.spec_versions("http://test-server.url")
+    base_url = "http://test-server.url"
+    request = Request.spec_versions(base_url)
 
     assert request.method == :get
-    assert request.base_url == "http://test-server.url"
+    assert request.base_url == base_url
     assert request.path == "/_matrix/client/versions"
   end
 
   test "server_discovery/1" do
-    request = Request.server_discovery("http://test-server.url")
+    base_url = "http://test-server.url"
+    request = Request.server_discovery(base_url)
 
     assert request.method == :get
-    assert request.base_url == "http://test-server.url"
+    assert request.base_url == base_url
     assert request.path == "/.well-known/matrix/client"
   end
 
   test "login/1" do
-    request = Request.login("http://test-server.url")
+    base_url = "http://test-server.url"
+    request = Request.login(base_url)
 
     assert request.method == :get
-    assert request.base_url == "http://test-server.url"
+    assert request.base_url == base_url
     assert request.path == "/_matrix/client/r0/login"
   end
 
   test "login/3" do
-    request = Request.login("http://test-server.url", "username", "password")
+    base_url = "http://test-server.url"
+    username = "username"
+    password = "password"
+    request = Request.login(base_url, username, password)
 
     assert request.method == :post
-    assert request.base_url == "http://test-server.url"
+    assert request.base_url == base_url
     assert request.path == "/_matrix/client/r0/login"
     assert request.body.type == "m.login.password"
-    assert request.body.user == "username"
-    assert request.body.password == "password"
+    assert request.body.user == username
+    assert request.body.password == password
   end
 
   test "logout/2" do
-    request = Request.logout("http://test-server.url", "token")
+    base_url = "http://test-server.url"
+    token = "token"
+    request = Request.logout(base_url, token)
 
     assert request.method == :post
-    assert request.base_url == "http://test-server.url"
+    assert request.base_url == base_url
     assert request.path == "/_matrix/client/r0/logout"
-    assert request.headers == [{"Authorization", "Bearer token"}]
+    assert request.headers == [{"Authorization", "Bearer " <> token}]
   end
 
   test "register_user/1" do
-    request = Request.register_user("http://test-server.url")
+    base_url = "http://test-server.url"
+    request = Request.register_user(base_url)
 
     assert request.method == :post
-    assert request.base_url == "http://test-server.url"
+    assert request.base_url == base_url
     assert request.path == "/_matrix/client/r0/register?kind=guest"
   end
 
   test "register_user/3" do
-    request = Request.register_user("http://test-server.url", "username", "password")
+    base_url = "http://test-server.url"
+    username = "username"
+    password = "password"
+    request = Request.register_user(base_url, username, password)
 
     assert request.method == :post
-    assert request.base_url == "http://test-server.url"
+    assert request.base_url == base_url
     assert request.path == "/_matrix/client/r0/register"
     assert request.body.auth.type == "m.login.dummy"
-    assert request.body.username == "username"
-    assert request.body.password == "password"
+    assert request.body.username == username
+    assert request.body.password == password
   end
 
   test "room_discovery/1" do
-    request = Request.room_discovery("http://test-server.url")
+    base_url = "http://test-server.url"
+    request = Request.room_discovery(base_url)
 
     assert request.method == :get
-    assert request.base_url == "http://test-server.url"
+    assert request.base_url == base_url
     assert request.path == "/_matrix/client/r0/publicRooms"
   end
 end
