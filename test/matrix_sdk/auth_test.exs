65eb109237c096a016834a56ddd7a5f31798a9ec
nkls <niklas@long.ch>
2020-06-09 15:42:57 +0200

    Add tests for Auth module

---
+++ test/matrix_sdk/auth_test.exs
@@ -0,0 +1,59 @@
+defmodule MatrixSDK.AuthTest do
+  use ExUnit.Case
+
+  alias MatrixSDK.Auth
+
+  test "login_dummy/0" do
+    assert Auth.login_dummy() == %{type: "m.login.dummy"}
+  end
+
+  test "login_token/1" do
+    token = "token"
+    auth = Auth.login_token(token)
+
+    assert auth.type == "m.login.token"
+    assert auth.token == token
+  end
+
+  test "login_user/2" do
+    user = "username"
+    password = "password"
+    auth = Auth.login_user(user, password)
+
+    assert auth.type == "m.login.password"
+    assert auth.password == password
+    assert auth.identifier.type == "m.id.user"
+    assert auth.identifier.user == user
+  end
+
+  test "login_3pid/3" do
+    medium = "medium"
+    address = "address"
+    password = "password"
+    auth = Auth.login_3pid(medium, address, password)
+
+    assert auth.type == "m.login.password"
+    assert auth.password == password
+    assert auth.identifier.type == "m.id.thirdparty"
+    assert auth.identifier.medium == medium
+    assert auth.identifier.address == address
+  end
+
+  test "login_phone/3" do
+    country = "CH"
+    phone = "phone"
+    password = "password"
+    auth = Auth.login_phone(country, phone, password)
+
+    assert auth.type == "m.login.password"
+    assert auth.password == password
+    assert auth.identifier.type == "m.id.phone"
+    assert auth.identifier.country == country
+    assert auth.identifier.phone == phone
+  end
+
+  test "put_session/2" do
+    session_id = "session ID"
+    assert Auth.put_session(%{}, session_id) == %{session: session_id}
+  end
+end
