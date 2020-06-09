605661d56fce264f8fa71addebeeaf920c8e9bae
nkls <niklas@long.ch>
2020-06-09 15:32:31 +0200

    Add specs for Auth module

--- lib/matrix_sdk/auth.ex
+++ lib/matrix_sdk/auth.ex
@@ -16,37 +16,48 @@ defmodule MatrixSDK.Auth do
   @type t :: dummy | token | password
   @type id :: id_user | id_thirdparty | id_phone
 
+  @spec login_dummy() :: dummy
   def login_dummy(), do: %{type: "m.login.dummy"}
+
+  @spec login_token(binary) :: token
   def login_token(token), do: %{type: "m.login.token", token: token}
 
+  @spec login_password(id, binary) :: password
   defp login_password(identifier, password),
     do: %{type: "m.login.password", identifier: identifier, password: password}
 
+  @spec login_user(binary, binary) :: password
   def login_user(user, password),
     do:
       user
       |> id_user()
       |> login_password(password)
 
+  @spec login_3pid(binary, binary, binary) :: password
   def login_3pid(medium, address, password),
     do:
       medium
       |> id_thirdparty(address)
       |> login_password(password)
 
+  @spec login_phone(binary, binary, binary) :: password
   def login_phone(country, phone, password),
     do:
       country
       |> id_phone(phone)
       |> login_password(password)
 
+  @spec id_user(binary) :: id_user
   defp id_user(user), do: %{type: "m.id.user", user: user}
 
+  @spec id_thirdparty(binary, binary) :: id_thirdparty
   defp id_thirdparty(medium, address),
     do: %{type: "m.id.thirdparty", medium: medium, address: address}
 
+  @spec id_phone(binary, binary) :: id_phone
   defp id_phone(country, phone),
     do: %{type: "m.id.phone", country: country, phone: phone}
 
+  @spec put_session(map, binary) :: map
   def put_session(map, session_id), do: Map.put(map, :session, session_id)
 end
