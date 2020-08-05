d29d87313b6488c89e5b6c20497a1b6df57b27b4
nkls <niklas@long.ch>
2020-08-05 15:54:46 +0200

    Implement RoomEvent m.room.message (text)

---
+++ lib/matrix_sdk/room_event.ex
@@ -0,0 +1,17 @@
+defmodule MatrixSDK.RoomEvent do
+  @enforce_keys [:content, :type, :transaction_id, :room_id]
+  defstruct [:content, :type, :room_id, :transaction_id]
+
+  def message(room_id, type, body),
+    do:
+      %__MODULE__{
+        content: content(type, body),
+        type: "m.room.message",
+        room_id: room_id,
+        transaction_id: transaction_id()
+      }
+
+  defp content(:text, body), do: %{msgtype: "m.text", body: body}
+
+  defp transaction_id(), do: "dostringthing"
+end
