95920b8226d6a886adffae7f5abeff77d6b39036
Niklas Long <niklaslong@users.noreply.github.com>
2020-08-11 08:51:40 +0200

    Remove transaction from RoomEvent (#59)

--- test/matrix_sdk/room_event_test.exs
+++ test/matrix_sdk/room_event_test.exs
@@ -2,19 +2,22 @@ defmodule MatrixSDK.RoomEventTest do
   use ExUnit.Case, async: true
   alias MatrixSDK.RoomEvent
 
+  doctest MatrixSDK.RoomEvent
+
   describe "message/3 with type:" do
     test "m.text" do
       room_id = "!someroom:matrix.org"
       type = :text
       body = "Fire! Fire! Fire!"
+      transaction_id = "transaction_id"
 
-      room_event = RoomEvent.message(room_id, type, body)
+      room_event = RoomEvent.message(room_id, type, body, transaction_id)
 
       assert room_event.content.msgtype == "m.text"
       assert room_event.content.body == body
       assert room_event.type == "m.room.message"
       assert room_event.room_id == room_id
-      assert is_binary(room_event.transaction_id)
+      assert room_event.transaction_id == transaction_id
     end
   end
 end
