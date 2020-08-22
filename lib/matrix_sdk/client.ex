489dc623817887c3ce46ec65b4dccadda0b036a7
Niklas Long <niklas@long.ch>
2020-08-22 10:45:29 +0200

    Add 3PID flows section

--- lib/matrix_sdk/client.ex
+++ lib/matrix_sdk/client.ex
@@ -4,6 +4,22 @@ defmodule MatrixSDK.Client do
   `MatrixSDK.Client.Request` and `MatrixSDK.HTTPClient` modules.
 
   ## 3PID API flows
+
+  See this [gist](https://gist.github.com/jryans/839a09bf0c5a70e2f36ed990d50ed928) for more details.
+
+  Flow 1—adding a 3PID to HS account during registration:
+  1. `registration_email_token/5` or `registration_msisdn_token/6`
+  2. `register_user/4`
+
+  Flow 2—adding a 3PID to HS account after registration:
+  1. `account_email_token/5` or `account_msisdn_token/6`
+  2. `account_add_3pid/5`
+
+    Flow 3—changing the bind status of a 3PID: this is currently unsupported but will be available once the identity server endpoints are wrapped.
+
+  Flow 4—reset password via email:
+  1. `password_email_token/5`
+  2. `change_password/4`
   """
 
   alias MatrixSDK.HTTPClient
