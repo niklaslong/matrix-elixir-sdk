487a0fa4478f214841a7f516b83387de3201c2db
inaniyants <igor.naniyants@gmail.com>
2020-09-11 10:17:01 +0300

    Add m.file, m.notice support for RoomEvent of type message, textual msgtypes are now accepting formatted_body

--- lib/matrix_sdk/client/room_event.ex
+++ lib/matrix_sdk/client/room_event.ex
@@ -9,7 +9,7 @@ defmodule MatrixSDK.Client.RoomEvent do
   @type t :: %__MODULE__{}
 
   @doc """
-  Returns a `RoomEvent` struct of type `m.room.message`. 
+  Returns a `RoomEvent` struct of type `m.room.message`.
 
   ## Example
 
@@ -22,13 +22,47 @@ defmodule MatrixSDK.Client.RoomEvent do
       }
 
   """
-  def message(room_id, type, body, transaction_id),
+  def message(room_id, type, content, transaction_id),
     do: %__MODULE__{
-      content: content(type, body),
+      content: content(type, content),
       type: "m.room.message",
       room_id: room_id,
       transaction_id: transaction_id
     }
 
+  defp content(:text, %{body: body, formatted_body: formatted_body, format: format}) do
+    %{
+      msgtype: "m.text",
+      body: body,
+      formatted_body: formatted_body,
+      format: format
+    }
+  end
+
+  defp content(:text, %{body: body, formatted_body: formatted_body}) do
+    content(:text, %{body: body, formatted_body: formatted_body, format: "org.matrix.custom.html"})
+  end
+
+  defp content(:text, %{body: body}), do: %{msgtype: "m.text", body: body}
+
   defp content(:text, body), do: %{msgtype: "m.text", body: body}
+
+  defp content(:notice, content) do
+    content(:text, content)
+    |> Map.update!(:msgtype, fn _ -> "m.notice" end)
+  end
+
+  @file_optional_params [:filename, :info]
+
+  defp content(:file, %{body: body, url: url} = content) do
+    acc = %{msgtype: "m.file", body: body, url: url}
+
+    Enum.reduce(content, acc, fn {param_name, value}, acc ->
+      if Enum.member?(@file_optional_params, param_name) do
+        Map.put(acc, param_name, value)
+      else
+        acc
+      end
+    end)
+  end
 end
